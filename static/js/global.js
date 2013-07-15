c5e6e14484267def28a1c786c91dd4c9081f9afd
shaotian.hu <shaotian.hu@travelzen.com>
2013-07-15 16:59:57 +0800

    碎片

--- static/js/global.js
+++ static/js/global.js
@@ -180,3 +180,236 @@ Collpase.prototype = {
 
     }
 }
+
+/* 城市补全js 模板
+
+<script type="text/x-kendo-template" id="city_popup_tpl">
+<div id="tabstrip" class="tabstrip" style="width:370px">
+<ul>
+<li id="hot_city" class="k-state-active">热门</li>
+# for (var i=0; i<data.group.length; i++ ) { #
+<li>#= data.group[i] #</li>
+# } #
+</ul>
+<div title="热门" id="hot_city_tab">
+<ul class="tcy_list clearfix">
+
+# for(var j=0;j<hotcitylist.length;j++){ #
+# var citylist_item = hotcitylist[j] #
+<li data-code="#= citylist_item.code #">#= citylist_item.name #</li>
+# } #
+
+</ul>
+</div>
+
+# for(var i=0;i<data.citylist.length;i++){ #
+# var citylist_item = data.citylist[i] #
+<div title="#= citylist_item.name #" class="city_tab">
+<ul class="tcy_list clearfix">
+# for(var j=0;j<citylist_item.value.length;j++){ #
+# var citylist_value_item = citylist_item.value[j] #
+<li data-code="#= citylist_value_item.code #">#= citylist_value_item.name #</li>
+# } #
+</ul></div>
+# } #
+</div>
+</script>
+
+模板结束 */
+
+var CityAutocomplete = function(){
+    var that = {};
+    var input,opts,pcitydata,hcitydata,ppy,hpy,citygroup,url,process_citydata;
+    var hot_tabs;
+    /* /FlightReserve/DataAssistant/GetCitys.aspx */
+
+    var posited_tabwindow = function(ele){
+        var pos = ele.offset();
+        hot_tabs.css({
+            display:'block',
+            position:'absolute',
+            left:pos.left - 2,
+            top:pos.top + 23
+        }).data('sugid',ele.attr('id'));
+    };
+
+    var process_pinyin = function(arr, group, col) {
+        var result = [],
+        len = group.length,
+        r = 0,
+        item;
+
+        for(; r<len; r++){
+            item = group[r];
+            result[r] = {
+                name:"",
+                value:[]
+            };
+            result[r]['name'] = group[r];
+            jQuery.each(arr, function(index,i) {
+                var str = i[col];
+                var first_letter = str.toUpperCase().charCodeAt(0);
+                var rangeStart = item.charCodeAt(0);
+                var rangeEnd = item.charCodeAt(group[r].length-1);
+
+                if (first_letter >= rangeStart && first_letter <= rangeEnd) {
+                    result[r]['value'].push(i);
+                }
+            });
+        }
+
+        return result;
+    };
+
+    var init_hotcity_tabs = function(data){
+        var templateID = opts.template || "#city_popup_tpl"; 
+        var city_popup_tpl = kendo.template($(templateID).html());
+        $('body').append(city_popup_tpl(data));
+        hot_tabs = $('#tabstrip');
+
+        hot_tabs.delegate('.tcy_list li[data-code]','click',function(){
+            hot_tabs.hide();
+            var $t = $(this);
+            var text = $t.text();
+            input.val(text);
+            $(opts.codeEle).val($t.data('code'));
+        });
+
+        hot_tabs.kendoTabStrip({
+            animation:false
+        });
+    };
+
+    var init_suggest_city = function(data){
+        input.kendoAutoComplete({
+            dataTextField:'search',
+            animation:false,
+            filter:function(d,f){
+                return {
+                    filters:[{
+                        ignoreCase: true,
+                        value:  d,
+                        operator: 'startswith',
+                        field: 'code'
+                    },{
+                        ignoreCase: true,
+                        value:  d,
+                        operator: 'startswith',
+                        field: 'py'
+                    },{
+                        ignoreCase: true,
+                        value:  d,
+                        operator: 'startswith',
+                        field: 'name'
+                    }],
+                    logic:'or'
+                }
+            },
+            template: $(opts.itemTemplate).html() ||'<span class="sg_py">${data.py}</span><span class="sg_name">${data.name}</span><span class="sg_code">（${data.code}）</span>',
+            dataSource: data,
+            highlightFirst: true,
+            placeholder:"拼音/城市码/中文",
+            select:opts.select || function(t){
+                var dataItem = this.dataItem(t.item.index());
+                t.preventDefault();
+                t.sender.value(dataItem.name);
+                $(opts.codeEle).val(dataItem.code);
+            }
+        });
+
+        input.on('focus',function(e){
+            var $t = $(this);
+            if($t.val() == '') {
+                var tabcity = hot_tabs.data('kendoTabStrip');
+                tabcity.activateTab($("#hot_city"));
+
+                if ( $t.data('kendoAutoComplete').popup.element[0].style.display !== 'block' ){
+                    posited_tabwindow($t);
+                }
+            }
+        });
+
+        var blur_event = function(e){
+            if (  e.relatedTarget && e.relatedTarget.id != "tabstrip" || e.relatedTarget === null){
+                hot_tabs.hide();
+            }
+        };
+
+        input.on('blur',blur_event);
+        hot_tabs.on('blur',blur_event);
+
+        input.on('keyup',function(){
+            var $t = $(this);
+            if( $t.val() === '' ){
+                posited_tabwindow($t);
+            }else{
+                hot_tabs.hide();
+            }
+        });
+
+        $.each(input,function(){
+            $(this).data('kendoAutoComplete').bind('open',function(){
+                hot_tabs.hide();
+            }).bind('close',function(e){
+                if(e.sender.value()===''){
+                    posited_tabwindow(e.sender.element);
+                }
+            });
+        });
+    };
+
+    that.setOptions = function(settings){
+        var defaults = {
+            input : $(".suggest-city"),
+            url: './searchflight_files/data.txt',
+            group: ["ABCD","EFGHJ", "KLMN", "PQRSTW", "XYZ"]
+        };
+
+        defaults.process_citydata = function(d){
+            return jQuery.map(d.split(';'),function(n){
+                if(n.indexOf('|')==-1) return;
+                var item = n.split('|');
+                return {'code':item[0],'search':item[0]+'|'+item[2]+'|'+item[1],'py':item[1],'name':item[2]};
+            });
+        }
+
+        that.options = $.extend(defaults,settings);
+        return that;
+    };
+
+    that.init = function(){
+        if(typeof that.options === 'undefined'){
+            that.setOptions(); 
+        }
+
+        opts = that.options;
+
+        input = opts.input;
+        citygroup = opts.group;
+        url = opts.url;
+        process_citydata = opts.process_citydata;
+
+        $.get(url,function(d){
+            var citydata = d.split('@');
+            pcitydata = process_citydata(citydata[1]);
+            hcitydata = process_citydata(citydata[0]);
+
+            citygroup.shift();
+            ppy = process_pinyin(pcitydata, citygroup, "py");
+
+            var data = {
+                "group": citygroup,
+                "citylist": ppy,
+                "hotcitylist": hcitydata
+            }
+
+            init_hotcity_tabs(data);
+            init_suggest_city(pcitydata);
+        });
+
+        return that;
+    };
+
+    return that;
+}();
+
