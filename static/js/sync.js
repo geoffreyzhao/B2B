ca38575ba08e24d3203704d89f3f52b8d84985a9
linbo.chen <linbo.chen@travelzen.com>
2013-08-01 18:19:40 +0800

    修复问题

--- static/js/sync.js
+++ static/js/sync.js
@@ -1,11 +1,14 @@
+function load_header(){
+    $(".header").load(encodeURI("/运营商/同步头.html"),{},function(){
+        init_header();
+    });
+}
 $(function(){
-
    if(typeof(init_header) == "undefined"){
        $.getScript("/static/js/header.js",function(){
-           $(".header").load(encodeURI("/运营商/同步头.html"),{},function(){
-               init_header();
-           });
+           load_header();
        });
+   }else{
+       load_header();
    }
-
 });
