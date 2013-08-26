c5f3132ebd5f99f48ed70fd2997724aaaea4977c
linbo.chen <linbo.chen@travelzen.com>
2013-08-26 13:42:36 +0800

    修改菜单

--- static/js/header.js
+++ static/js/header.js
@@ -1,23 +1,32 @@
 function init_header(){
+    currentMenuIndex = $("#top_menu > li.current").index();
+    //菜单事件设置
     $("#top_menu > li").each(function(index){
+        var obj = $("#top_menu > li.current");
         $(this).hover(function(){
-            $(this).addClass("mnav");
-        },function(){
-            $(this).removeClass("mnav");
-        });
-        $(this).click(function(){
-            var refresh = true;
-            if($(this).hasClass("current")){
-                refresh = false;
+            //记住当前是那个菜单
+            if(currentMenuIndex < 0) {
+                currentMenuIndex = $("#top_menu > li.current").index();
             }
-            $("#top_menu > li").removeClass("current");
-            $(this).addClass("current");
 
-            if(refresh){
-                $(".sub_menu").hide();
-                $(".sub_menu",this).fadeIn(); 
+            //$("#top_menu > li:eq(" + currentMenuIndex + ")").removeClass("current");
+
+            $("#top_menu > li").removeClass("mnav");
+            if(!$(this).hasClass("current")){
+                $(this).addClass("mnav");
             }
+            $(".sub_menu").hide();
+            $(".sub_menu",this).show();
+        },function(){
+
         });
+    });
+
+    $("#top_menu").mouseleave(function(){
+        $("#top_menu > li").removeClass("mnav");
+        $(".sub_menu").hide();
+        $("#top_menu > li:eq(" + currentMenuIndex + ")").addClass("current");
+        $("#top_menu > li:eq(" + currentMenuIndex + ") .sub_menu").show();
     });
 
     $("#message h3 a,#message h3 span").mouseover(function(){
