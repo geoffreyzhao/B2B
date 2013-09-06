f27906f126b53ec5e6a0416e84b9c5bc4f13526f
linbo.chen <linbo.chen@travelzen.com>
2013-09-06 17:59:48 +0800

    修改

---
+++ static/js/plugins/switcher.js
@@ -0,0 +1,24 @@
+$(function(){
+
+
+    var switcher = $("<div><select name=''></select></div>");
+
+    var $backToTopTxt = "返回顶部", $backToTopEle = $('<a class="backToTop"></a>').appendTo($("body"))
+        .text($backToTopTxt).attr("title", $backToTopTxt).click(function() {
+            $("html, body").animate({ scrollTop: 0 }, 120);
+    }) ;
+
+    $backToTopFun = function() {
+        var st = $(document).scrollTop(), winh = $(window).height();
+        (st > 0)? $backToTopEle.show(): $backToTopEle.hide();    
+        //IE6下的定位
+        if (!window.XMLHttpRequest) {
+            $backToTopEle.css("top", st + winh - 166);    
+        }
+    };
+
+
+    $(window).bind("scroll", $backToTopFun);
+    $(function() { $backToTopFun(); });
+
+});
