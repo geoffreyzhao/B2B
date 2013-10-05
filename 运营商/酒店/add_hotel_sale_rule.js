cd1ecbd8b7dda119df087cf085ea371314991418
linbo.chen <linbo.chen@travelzen.com>
2013-10-05 20:11:28 +0800

    修改 弹窗

--- 运营商/酒店/add_hotel_sale_rule.js
+++ 运营商/酒店/add_hotel_sale_rule.js
@@ -8,108 +8,153 @@ Array.prototype.inArray = function(p_val){
 	return false;
 
 };
-var hotel_dataSource = new kendo.data.DataSource({
+var dataSource = {};
+
+dataSource.hotel = new kendo.data.DataSource({
 		data: [
         {
-			hotel_id: 1,
+			id: 1,
 			city: "上海1",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 2,
+			id: 2,
 			city: "上海2",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 3,
+			id: 3,
 			city: "上海3",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 4,
+			id: 4,
 			city: "上海4",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 5,
+			id: 5,
 			city: "上海5",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 6,
+			id: 6,
 			city: "上海6",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 7,
+			id: 7,
 			city: "上海7",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 8,
+			id: 8,
 			city: "上海8",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 9,
+			id: 9,
 			city: "上海9",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 10,
+			id: 10,
 			city: "上海10",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		},
         {
-			hotel_id: 11,
+			id: 11,
 			city: "上海11",
 			position: "闸北区",
-			hotel_name: "宝华国际酒店",
+			name: "宝华国际酒店",
 			star: "5",
 			brand: "宝华国际",
-			sign_manager: "张三",
+			sign_manager: "张三"
 		}
 
+        ],
+        page:1,
+        pageSize: 2
+	}
+);
+dataSource.customer = new kendo.data.DataSource({
+		data: [
+        {
+			id: 1,
+			city: "上海1",
+			name: "大有票务1",
+			create_time: "2013-02-01",
+			account_type: "月结",
+			blance: "20000.00",
+            link_man:"张三"
+		},
+        {
+			id: 2,
+			city: "上海2",
+			name: "大有票务2",
+			create_time: "2013-02-01",
+			account_type: "月结",
+			blance: "20000.00",
+            link_man:"张三"
+		},
+        {
+			id: 3,
+			city: "上海3",
+			name: "大有票务3",
+			create_time: "2013-02-01",
+			account_type: "月结",
+			blance: "20000.00",
+            link_man:"张三"
+		},
+        {
+			id: 4,
+			city: "上海4",
+			name: "大有票务4",
+			create_time: "2013-02-01",
+			account_type: "月结",
+			blance: "20000.00",
+            link_man:"张三"
+		}
         ],
         page:1,
         pageSize: 2
@@ -128,32 +173,33 @@ var filter_win = { };
 var exclude_item_tpl = kendo.template($("#exclude_item_tpl").html());  
 
 function select_item(obj){
-    var data = hotel_dataSource.data(); 
-    var data_offset = (hotel_dataSource.page() - 1) * hotel_dataSource.pageSize();
+    var self_name = $(obj).attr("name"); 
+    var ds = dataSource[self_name];
+    var data = ds.data(); 
+    var data_offset = (ds.page() - 1) * ds.pageSize();
     var index = $(obj).closest("tr").index() + data_offset;
     var item = data[index]; 
-    var self_name = $(obj).attr("name"); 
 
     var f = $(obj).closest(".filter").find(".filtered_data");
 
     if($(obj).prop("checked")){
-        var node = exclude_item_tpl({id:item.hotel_id,name:self_name,hotel_name:item.hotel_name,hotel_position:item.position});
-        if(!filterd_data[self_name].inArray(item.hotel_id)){
-            filterd_data[self_name].push(item.hotel_id);
+        var node = exclude_item_tpl({id:item.id,group:self_name,name:item.name,city:item.city});
+        if(!filterd_data[self_name].inArray(item.id)){
+            filterd_data[self_name].push(item.id);
             f.append(node);
         }
     }else{
-        if(filterd_data[self_name].inArray(item.hotel_id)){
+        if(filterd_data[self_name].inArray(item.id)){
 
             for(var i =0,l = filterd_data[self_name].length; i < l; i++){
-                if(filterd_data[self_name][i] == item.hotel_id){
+                if(filterd_data[self_name][i] == item.id){
                     break;
                 }
             }
             var a = filterd_data[self_name].slice(0,i);
             var b = filterd_data[self_name].slice(i + 1);
             filterd_data[self_name] = a.concat(b); 
-            $("#" + self_name + item.hotel_id).remove();
+            $("#" + self_name + item.id).remove();
         }
     }
 
@@ -161,7 +207,6 @@ function select_item(obj){
 };
 
 $(function() {
-	var hotel_table;
 
 	filter_win["hotel"] = new PopWindow("#filter_hotel", {
 		title: "勾选排除酒店",
@@ -172,7 +217,7 @@ $(function() {
         },
 		open: function() {
 			hotel_table = new GridTable('#hotel_list', {
-				dataSource:hotel_dataSource,
+				dataSource:dataSource['hotel'],
                 dataBounding:function(e){
                 },
 				dataBound: function(e) {
@@ -185,6 +230,32 @@ $(function() {
 					filter_win["hotel"].center();
 				},
 				rowTemplate: kendo.template($("#hotel_row_tpl").html())
+			}).init();
+		}
+	}).init();
+
+	filter_win["customer"] = new PopWindow("#filter_customer", {
+		title: "勾选排除客户",
+		width: 730,
+		template: "#filter_customer_tpl",
+        activate:function(){
+            //
+        },
+		open: function() {
+			hotel_table = new GridTable('#customer_list', {
+				dataSource:dataSource['customer'],
+                dataBounding:function(e){
+                },
+				dataBound: function(e) {
+                    $(".filter tbody input[name=customer]").each(function(index){
+                        if(filterd_data["customer"].inArray($(this).val())){
+                            $(this).prop("checked",true);
+                        }
+                    });
+
+					filter_win["customer"].center();
+				},
+				rowTemplate: kendo.template($("#customer_row_tpl").html())
 			}).init();
 		}
 	}).init();
@@ -206,7 +277,6 @@ $(function() {
         var index = $(this).index();
         var name = $(this).attr("name");
         var group = name.substring(0,name.indexOf('_'));
-        console.log(group,index)
 
         if(0 == index){
             //保存按钮
