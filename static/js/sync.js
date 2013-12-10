534700f160f4eaa371a3f7b195434cf9bf1f4d11
linbo.chen <linbo.chen@travelzen.com>
2013-12-10 17:03:28 +0800

    修改返回顶部
    Change-Id: I0a39889d5fda8ed0ab7f0d7847c9502c79bf88b6

--- static/js/sync.js
+++ static/js/sync.js
@@ -43,7 +43,7 @@ $(function() {
     var w = $("#container").width();
     */
 	var $backToTopTxt = "返回顶部",
-	$backToTopEle = $('<a class="backToTop"></a>').appendTo($("body")).text($backToTopTxt).attr("title", $backToTopTxt).click(function() {
+	$backToTopEle = $('<a class="backToTop"></a>').appendTo($("#container")).text($backToTopTxt).attr("title", $backToTopTxt).click(function() {
 		$("html, body").animate({
 			scrollTop: 0
 		},
@@ -52,7 +52,7 @@ $(function() {
 	$backToTopFun = function() {
 		var st = $(document).scrollTop(),
 		winh = $(window).height();
-		(st > 0) ? $backToTopEle.show() : $backToTopEle.hide();
+		(st > 800) ? $backToTopEle.show() : $backToTopEle.hide();
 		//IE6下的定位
 		if (!window.XMLHttpRequest) {
 			$backToTopEle.css("top", st + winh - 166);
