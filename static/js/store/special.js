2d99d26f8e6a27642acadacc4125e0e908ec7b51
yan.liu <yan.liu@travelzen.com>
2014-04-19 18:51:33 +0800

    update 桂林
    Change-Id: I3e8fd0693551b6dad1c0f45a4349e8054c7b8904

---
+++ static/js/store/special.js
@@ -0,0 +1,19 @@
+$(function(){
+    var ele=$('.banner_xiamen_left');
+    $(window).on('resize',function(){
+        var winWidth=window.innerWidth;
+    
+        if(winWidth<1600){
+            ele.css({
+                left:parseInt((window.innerWidth-1600)/1.6)
+            });
+        }
+    });
+
+    if(window.innerWidth<1600){
+        ele.css({
+            left:parseInt((window.innerWidth-1600)/1.6)
+        });
+    }
+
+});
