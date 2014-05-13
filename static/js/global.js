f419b35256ed1c272cb2edcf1a39a20fda875061
shaotian.hu <shaotian.hu@travelzen.com>
2014-05-13 12:34:18 +0800

    update
    Change-Id: If2bdf6a36406ec643f729081b1defd6ef64e3396

--- static/js/global.js
+++ static/js/global.js
@@ -1,3 +1,12 @@
+function loadCss(url) {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = url;
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+
 //弹窗封装
 function PopWindow(trigger, customSettings, window_id){
     var kendoWinDefaults = {
