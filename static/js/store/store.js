2dfc8bba4df8bbca8dfb6ceea32d5e1c95ffb5fa
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2014-07-25 11:14:29 +0800

    remove js code from store and import new js file
    Change-Id: I83c26b832725e5c9aa44eea59e1ec6b7be09ce3f

--- static/js/store/store.js
+++ static/js/store/store.js
@@ -235,36 +235,5 @@ $(function() {
     })();
     //var conf_enter = $('.main-header').append('<a href="'+conf_enter_url+'" class="conf_enter_wrapper"><i></i><s></s></a>');
 
-    // Gif动画logo执行30秒后，切换为静态logo，3分钟后再次切换回动画，依此循环
-    // 获取图片所在根目录
-    // var dirArr = $("#headLogo").attr("src").split('/');
-    // var parentDir = dirArr.slice(0, dirArr.length - 1).join('/');
-
-    // // 切换效果 start
-    // function switchLogo() {
-
-    //     $("#headLogo").attr("src", parentDir + "/newLogo.png");
-    //     $("#headLogo").addClass("headStaticLogo");
-
-    //     setTimeout(function() {
-    //         $("#headLogo").attr("src", parentDir + "/logoGif.gif");
-    //         $("#headLogo").removeClass("headStaticLogo");
-    //     }, 1000*3*60);
-    // }
-
-    // setTimeout(function(){
-    //     switchLogo();
-    //     setInterval(switchLogo, 1000*3.5*60);
-    // }, 1000*0.5*60);
-
-    // // 切换效果 end
-
-    // // 为gif logo添加点击事件，跳转至app介绍.html
-    // $("#headLogo").click(function(){
-    //     if ($(this).attr("src") == parentDir + "/logoGif.gif") {
-    //         window.location.href = "/采购商/app介绍.html";
-    //     }
-    // });
-
 });
 
