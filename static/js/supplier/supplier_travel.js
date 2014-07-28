c389b2f3e881704f2f42f563ce3ba74678563704
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2014-07-28 10:17:37 +0800

    remove travel_header js
    Change-Id: I143c011fcec0312ca329fbbad1f00cf6509d8931

--- static/js/supplier/supplier_travel.js
+++ static/js/supplier/supplier_travel.js
@@ -1,6 +1,48 @@
 // 新版供应商  -- 旅游模块
 $(function(){
 
+	// header common js start
+	$(".first-navi-li").each(function(index) {
+		$(this).click(function(){
+			$(this).addClass("first-navi-active");
+			$(this).siblings("li").removeClass('first-navi-active');
+		});
+	});
+
+	$(".navi-slide-hover").hide();
+
+	$(".second-navi-li").each(function() {
+		$(this).hover(function(){
+			$(this).children(".navi-slide-hover").show('600');
+		}, function(){
+			$(this).children('.navi-slide-hover').hide('600');
+		});
+
+		$(this).click(function() {
+			$(this).children(".navi-slide-hover").show();
+			$(this).siblings().children('.navi-slide-hover').hide();
+		});
+	});
+
+	$(".setting-content").hide();
+
+	$(".setting-icon").hover(function(){
+		$(this).addClass("setting-icon-active");
+		$(".setting-content").show();
+	}, function(){
+		$(this).removeClass("setting-icon-active");
+		$(".setting-content").mouseout();
+	});
+
+	$(".setting-content").hover(function() {
+		$(".setting-content").show();
+	}, function(){
+		$(".setting-content").hide();
+	});
+
+	// header common js end
+
+
 	// 左侧边栏 hover 改变样式
 	$(".menu-item-normal").hover(function(){
 		$(this).children(".menu-item-num-normal").addClass('menu-item-num-hover');
