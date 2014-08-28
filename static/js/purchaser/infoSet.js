c10a44166ff3a89b641da2c2a18cb60e4304a52e
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2014-08-28 21:15:44 +0800

    Revert "pre revert some commits"
    Change-Id: Iba8a2f21f989abfe6c17f782fb2ddca5f90f3cf3

--- static/js/purchaser/infoSet.js
+++ static/js/purchaser/infoSet.js
@@ -16,67 +16,10 @@ $(function(){
 	});
 
 
-	// var addModelBtn = $('#modelList').find('.addModel');
-	// addModelBtn.trigger('click');
 
-	var modelInfo = $('.contentRight').find('span');
-	var modelTxtarea = $('.contentCenter').find('textarea');
-	modelInfo.bind('click',function(){
-		var _modelPoint = this;
-		modelTxtarea.val(function(){
-		 	return this.value+$(_modelPoint).text();
-		 });
-	});
 
-	var errorInfo = $('#errorInfo');
-	var modelTitle = $('.modelTitlePos2');
-		// modelTitle.on('blur',function(){
-		// 	var titleNum = $(this).val().length;
-		// 	if(0 == titleNum)
-		// 	{
-		// 		errorInfo.show();
-		// 		$(this).css('border','1px solid red');
-		// 	}
-				
-		// });
+
+
+
 
 });
-
-function setWordNumText() {
-	var wordNum = $('.contentCenter').find('textarea').val().length;
-    var messageNum = wordNum % 60 == 0 ? wordNum / 60 : parseInt(wordNum / 60) + 1
-	$(".wordNum").text(wordNum);
-	$(".infoNum").text(messageNum);
-}
-
-function getModleTitleFontNum(){
-
-	var modelTitle = $('.modelTitlePos2');
-	var errorInfo = $('#errorInfo');
-	var modelFontNum = modelTitle.val().length;
-	if(modelFontNum > 0)
-	{
-		errorInfo.hide();
-		modelTitle.css('border','1px solid #abadb3');
-	}
-}
-
-
-
-function getMsgLength(){
-        var modelTitle = $('.messageConTextarea textarea');//焦点得到 textarea取消报错
-        var errorInfo = $('.errorsTips');
-        var modelFontNum = modelTitle.val().length;
-        if(modelFontNum > 0)
-        {
-            errorInfo.hide();
-            modelTitle.css('border','1px solid #abadb3');
-        }
-
-        wordNum = $(".messageConTextarea textarea").val().length;
-        messageNum = wordNum % 60 == 0 ? wordNum / 60 : parseInt(wordNum / 60) + 1;
-        setWordNumText(wordNum,messageNum);
-
-        $(".word-num").text(wordNum);
-        $(".message-num").text(messageNum);
-};
