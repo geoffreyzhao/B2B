9116a54e1a8005b7c39a6d46c78a94d5df85b71d
weiwei2.gong <weiwei.gong@travelzen.com>
2014-10-22 13:52:37 +0800

    供应商/邮轮/航线管理/新增航线-描述信息.html修改
    Change-Id: Ib861b5e3db24535315913653e47124a93d35ace0

--- static/js/supplier/textboxList.js
+++ static/js/supplier/textboxList.js
@@ -1,38 +1,52 @@
-// $$ = jQuery;
-
-// $$(function(){
-	
-// 	var t = new TextboxList('form_tags_input', {});
-	
-// 	var slideDownBtn = $$(".hotTagSlideBtn");
-// 	var hotTag = $$(".hotTagTable .hotTagTableShow").find("div");
-// 	var hotTagAll = $$(".hotTagTable td").find("div");
-// 	var tagInput = $$(".tagInput");
-
-// 	var flag = 1;
-	
-// 	hotTagAll.on("click", function(){
-// 		var tempVal = tagInput.val();
-// 		t.add($$(this).text());
-// 	});	
-// });
-
-
-jQuery(function($){ 
-    var t = new TextboxList('form_tags_input', {});
-
-	var slideDownBtn = $(".hotTagSlideBtn");
+$(function(){
+	var slideDownBtn = $("#hotTagSlideBtn-arrow").find("i");
 	var hotTag = $(".hotTagTable .hotTagTableShow").find("div");
 	var hotTagAll = $(".hotTagTable td").find("div");
 	var tagInput = $(".tagInput");
 
 	var flag = 1;
+	slideDownBtn.on("click",function(){
+		if(1 == flag)
+		{
+			hotTag.slideDown();
+		    $(this).removeClass();
+	 		$(this).addClass("hotTagSlideUp");
+	 		flag = 2;
+		}
+		else if(2 == flag){
+			hotTag.slideUp();
+			$(this).removeClass();
+	 		$(this).addClass("hotTagSlideBtn");
+	 		flag = 1;
+		}
+	});
+
+
+
+	var tagsPlugin = $("#form_tags_input").tagsInput({
+    					"width":"790px",
+    					"height":"26px",
+    					"maxChars":"6"
+    					});
+
+	// var hotTag = $(".hotTagTable .hotTagTableShow").find("div");
+	// var hotTagAll = $(".hotTagTable td").find("div");
+	// var tagInput = $(".tagInput");
+
+	var flag = 1;
 	
 	hotTagAll.on("click", function(){
 		var tempVal = tagInput.val();
-		t.add($(this).text());
+		tagsPlugin.addTag($(this).text());
 	});	 
 });
+
+
+
+
+
+
+
 
 
 
