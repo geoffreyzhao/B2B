df650e14aabe50261be71bbcd4ea15e35ab0f8fa
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2015-01-23 11:23:13 +0800

    调整邮轮船只背景平铺
    Change-Id: I2c2c39a8b03eaeca15583c799dfa3becd05dfdbd

--- static/js/header.js
+++ static/js/header.js
@@ -86,3 +86,27 @@ $(function(){
 
     /** 返回顶部结束 */
 });
+
+function adjustBodyBg(param) {
+    if (param !== undefined) {
+        var windowWidth = $(window).outerWidth();
+        windowWidth = (windowWidth < 980) ? 980 : windowWidth;
+        var resetLeft = (windowWidth - 980)/2;
+
+        if (typeof(param) == "string") {
+            $(param).css({
+                "width": windowWidth + "px",
+                "margin-left": -resetLeft + "px"
+            });
+        } else if (param instanceof Array) {
+            for (var i = 0; i < param.length; i++) {
+                $(param[i]).css({
+                    "width": windowWidth + "px",
+                    "margin-left": -resetLeft + "px"
+                });
+            }
+        }
+    } else {
+        console.log("adjust window width no params");
+    }
+}
