502a9d76602d696e673682ed3b5b1481eed3ca98
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2015-01-30 14:09:33 +0800

    修改图片路径 & 完善官网页面问题
    Change-Id: Iaa23db3156fbdaccffd1cf8e3a743f8a283908a0

--- 真旅网官网/js/common.js
+++ 真旅网官网/js/common.js
@@ -12,6 +12,42 @@ $(function(){
             (st > 100) ? $backToTopEle.show() : $backToTopEle.hide();
         };
     $(window).bind("scroll", $backToTopFun);
+
+    /* 返回顶部  结束  */
+
+    $(".website").hover(function(){
+        $(".websiteList").show();
+    }, function(event){
+        var websiteX1 = $(".websiteList").offset().left,
+            websiteY1 = $(".websiteList").offset().top,
+            websiteX2 = websiteX1 + $(".websiteList").outerWidth(),
+            websiteY2 = websiteY1 + $(".websiteList").outerHeight();
+
+        if (event.clientX > websiteX2 || event.clientX < (websiteX1 - 5)
+            || event.clientY > websiteY2 || event.clientY < (websiteY1 - 5)) {
+            $(".websiteList").hide();
+        }
+    });
+
+    $(".websiteList").mouseout(function(){
+        var websiteX1 = $(".websiteList").offset().left,
+            websiteY1 = $(".websiteList").offset().top,
+            websiteX2 = websiteX1 + $(".websiteList").outerWidth(),
+            websiteY2 = websiteY1 + $(".websiteList").outerHeight();
+
+
+        if (event.clientX > (websiteX2 - 5) || event.clientX < websiteX1
+            || event.clientY > (websiteY2 - 5) || event.clientY < websiteY1) {
+            $(".websiteList").hide();
+        }
+        
+    });
+
+    $("#b2b-website").click(function(){
+        window.open("http://www.tdxinfo.com");
+    });
     
+    $("#b2g-website").click(function(){
+        window.open("http://vipcorp.travelzen.com");
+    });
 });
-/* 返回顶部  结束  */
