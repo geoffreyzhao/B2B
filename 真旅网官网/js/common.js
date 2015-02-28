b6fe36ca79de568105c2d8b2b8854632bab4e0af
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2015-02-28 18:07:20 +0800

    添加手机版
    Change-Id: I60cadf4c353a014e4c71bb7015a8b5fa3005bc79

--- 真旅网官网/js/common.js
+++ 真旅网官网/js/common.js
@@ -14,43 +14,4 @@ $(function(){
     $(window).bind("scroll", $backToTopFun);
 
     /* 返回顶部  结束  */
-
-    /*  旗下网站 hover事件，不需要
-    $(".website").hover(function(){
-        $(".websiteList").show();
-    }, function(event){
-        var websiteX1 = $(".websiteList").offset().left,
-            websiteY1 = $(".websiteList").offset().top,
-            websiteX2 = websiteX1 + $(".websiteList").outerWidth(),
-            websiteY2 = websiteY1 + $(".websiteList").outerHeight();
-
-        if (event.clientX > websiteX2 || event.clientX < (websiteX1 - 5)
-            || event.clientY > websiteY2 || event.clientY < (websiteY1 - 5)) {
-            $(".websiteList").hide();
-        }
-    });
-
-    $(".websiteList").mouseout(function(){
-        var websiteX1 = $(".websiteList").offset().left,
-            websiteY1 = $(".websiteList").offset().top,
-            websiteX2 = websiteX1 + $(".websiteList").outerWidth(),
-            websiteY2 = websiteY1 + $(".websiteList").outerHeight();
-
-
-        if (event.clientX > (websiteX2 - 5) || event.clientX < websiteX1
-            || event.clientY > (websiteY2 - 5) || event.clientY < websiteY1) {
-            $(".websiteList").hide();
-        }
-        
-    });
-    */
-    /* 旗下网站链接
-    $("#b2b-website").click(function(){
-        window.open("http://www.tdxinfo.com");
-    });
-    
-    $("#b2g-website").click(function(){
-        window.open("http://vipcorp.travelzen.com");
-    });
-    */
 });
