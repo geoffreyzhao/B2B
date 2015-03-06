8c7002039245a75f77c6c1ba29a62d97bbce06bd
jenny.wang <jenny.wang@travelzen.com>
2015-03-06 16:51:02 +0800

    修改样式适配主流手机
    Change-Id: I92a02134b9584e5f44b888292006680ffde9d6cd

--- 真旅网官网-手机版/js/common.js
+++ 真旅网官网-手机版/js/common.js
@@ -1,7 +1,16 @@
 $(function(){
 
     /*  头部弹出菜单操作  开始  */
-
+    
+    var navShow = false;
+    $(".subNavSpan").tap(function(){
+        if (navShow) {
+            $(".subNavDiv").hide();
+        } else {
+            $(".subNavDiv").show();
+        }
+        navShow = !navShow;
+    });
 
     /*  头部弹出菜单操作  结束  */
 
