3b5ffe4957263d192e3418638b5f6ca05a710642
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2015-03-18 14:17:47 +0800

    测试弹窗
    Change-Id: Ia8b557f86566d3a084079b035e32399d7bb84f0c

---
+++ 天猫预约-移动端/js/common.js
@@ -0,0 +1,18 @@
+$(function(){
+	var root = document.getElementsByTagName('html')[0],
+		    NATIVE_W = 640,
+		    NATIVE_H = 1136;
+
+	$(window).load(function(){
+		var cw = 20,
+	        w = window.innerWidth,
+	        h = window.innerHeight;
+
+		if ((w / h) > (NATIVE_W / NATIVE_H)) {
+			cw = h / (NATIVE_H / 40);
+		} else {
+			cw = w / (NATIVE_W / 40);
+		}
+		root.style.fontSize = cw + 'px';
+	});
+});
