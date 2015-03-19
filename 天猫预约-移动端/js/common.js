7210c3ac6dea97ff0e9e7b6e5257014adb1727ad
shuaigeng.zhao <shuaigeng.zhao@travelzen.com>
2015-03-19 10:59:54 +0800

    修改commonjs width判断 & 添加密码强度
    Change-Id: I0f3e7910a69112db85ebcc121f43a40090d4a953

--- 天猫预约-移动端/js/common.js
+++ 天猫预约-移动端/js/common.js
@@ -1,6 +1,7 @@
 /*  设置全局 html font-size  */
 var root = document.getElementsByTagName("html")[0];
-root.style.fontSize = (window.innerWidth / 320) * 20 + "px";
+var w = window.innerWidth >= 640 ? 640 : window.innerWidth;
+root.style.fontSize = (w / 320) * 20 + "px";
 
 $(function(){
 
